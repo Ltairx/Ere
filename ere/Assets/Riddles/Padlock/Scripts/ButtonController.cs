5b5a02f77bcd1a97ef59a5c8ba804b603bd1e9fd
Oruniarz <matimx2001@wp.pl>
2023-03-07 15:34:30 +0100

    Druga wersja kłódki
    Przyciski i display działa

--- ere/Assets/Riddles/Padlock/Scripts/ButtonController.cs
+++ ere/Assets/Riddles/Padlock/Scripts/ButtonController.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
-public class ButtonController : MonoBehaviour
+public class ButtonController : Riddle
 {
-
-    public TMP_Text liczba_tekst;
-    int n = 1;
+    public TMP_Text blok;
+    public TMP_Text trans;
+    private int n = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,38 @@ public class ButtonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(n>9)
+        if (n > 9)
         {
             n = 1;
         }
-        liczba_tekst.text = n.ToString();
+        if (n < 1)
+        {
+            n = 9;
+        }
+        blok.text = n.ToString();
+        trans.text = n.ToString();
+    }
+
+    public override Delegate GetFunction(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                return (Action<float>)Decrement;
+            case 1:
+                return (Action<float>)Increment;
+            default: 
+                return null;
+        }
+    }
+
+    private void Increment(float not_used)
+    {
+        n++;
+    }
+
+    private void Decrement(float not_used)
+    {
+        n--;
     }
 }
