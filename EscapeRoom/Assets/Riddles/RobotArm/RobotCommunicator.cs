b68825b33eaa230cd8478ef43c179bce48daee26
Oruniarz <matimx2001@wp.pl>
2023-03-24 19:48:55 +0100

    Prototyp Ramienia

---
+++ EscapeRoom/Assets/Riddles/RobotArm/RobotCommunicator.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class RobotCommunicator : Riddle
+{
+    // Start is called before the first frame update
+    public GameObject Base;
+    public GameObject Arm1;
+    public GameObject Arm2;
+    public GameObject Finger1;
+    public GameObject Finger2;
+
+    public TMP_Text height;
+    public TMP_Text rotation;
+    public TMP_Text fingers;
+
+    private int rotation_value = 30;
+    private int height_value = 10;
+    private int fingers_state = 0;
+
+    private int[,] queue;
+    private int iterator = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        if(iterator>13)
+        {
+            iterator = 13;
+        }
+        if(rotation_value>360)
+        {
+            rotation_value = 0;
+        }
+        height.text = iterator.ToString();
+    }
+    public override Delegate GetFunction(int index)
+    {
+        switch (index)
+        {
+            case 0: //wpisanie komendy height
+                return (Action<float>)height_command; 
+            case 1: //wpisanie komendy rotation
+                return (Action<float>)rotation_command;
+            case 2: //wpisanie komendy finger
+                return (Action<float>)fingers_command;
+            //case 3: //start
+            //    return (Action<float>)Decrement;
+            //case 4: //stop
+            //    return (Action<float>)Decrement;
+            //case 5: //reset
+            //    return (Action<float>)Decrement;
+            //case 6: //del_line
+            //    return (Action<float>)Decrement;
+            default:
+                return null;
+        }
+    }
+    void height_command(float not_used)
+    {
+        queue[iterator, 0] = 0;
+        queue[iterator, 1] = height_value;
+        iterator++;
+    }
+    void rotation_command(float not_used)
+    {
+        queue[iterator, 0] = 1;
+        queue[iterator, 1] = rotation_value;
+        iterator++;
+    }
+    void fingers_command(float not_used)
+    {
+        queue[iterator, 0] = 2;
+        queue[iterator, 1] = fingers_state;
+        iterator++;
+    }
+    //yield return null
+    //while(time.time<time.time+
+}
---
+++ EscapeRoom/Assets/Riddles/RobotArm/Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : Riddle
+{
+
+    public GameObject kostka;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Coroute());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override Delegate GetFunction(int index)
+    {
+        switch (index)
+        {
+            case 0: //wpisanie komendy height
+                return (Action<float>)function;
+            default:
+                return null;
+        }
+    }
+
+    void function(float not_used)
+    {
+        
+    }
+
+    IEnumerator Coroute()
+    {
+        float startTime = Time.time;
+        float duration = 0.9f;
+        Vector3 startRot = kostka.transform.localRotation.eulerAngles;
+        Vector3 endRot = new Vector3(0, 30, 0);
+        Vector3 obrot;
+
+        while (Time.time < startTime + duration)
+        {
+            float procent = (Time.time - startTime) / duration;
+            obrot = Vector3.Lerp(startRot, endRot, procent);
+            kostka.transform.localRotation = Quaternion.Euler(obrot);
+            yield return null;
+        }
+    }
+}
