57210c1e67e4f240ec9ca1b65693d5bc74085670
Ltairx <ltairxaltezi@gmail.com>
2023-04-03 23:33:52 +0200

    dodane mini audytorium

---
+++ EscapeRoom/Assets/Scripts/Interactable/Switcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Switcher: Switcher<float> { }
+
+public class Switcher<T> : Interactable<T>
+{
+    [field: SerializeField] private GameObject mainObject;
+    [field: SerializeField] private GameObject secondaryObject;
+    
+    protected override void OnStartInteract(Hand hand)
+    {
+        if (!mainObject.activeSelf)
+            secondaryObject.SetActive(false);
+        
+        mainObject.SetActive(!mainObject.activeSelf);
+    }
+}
