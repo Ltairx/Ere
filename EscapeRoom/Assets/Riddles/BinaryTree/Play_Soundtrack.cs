dd2f9aa97e5d2a4ba1661d0f7d857154bc69baf1
Workraft <ful@onet.eu>
2023-04-19 17:57:02 +0200

    Dźwięki
    Dodanie dźwięków do drzewa oraz do pokoju

---
+++ EscapeRoom/Assets/Riddles/BinaryTree/Play_Soundtrack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_Soundtrack : MonoBehaviour
+{
+    public AudioSource src1;
+    public AudioClip sfx_1;
+    bool in_room;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        src1.clip = sfx_1;
+        src1.volume = 0;
+        src1.Play();
+        in_room = false;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //src1.volume = 1;
+            in_room=true;
+        }   
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //src1.volume = 0;
+            in_room = false;
+        }
+    }
+    private void Update()
+    {
+        if(in_room == true && src1.volume < 1)
+        {
+            src1.volume += 0.01f;
+        }
+        if (in_room == false && src1.volume > 0)
+        {
+            src1.volume -= 0.01f;
+        }
+    }
+}
