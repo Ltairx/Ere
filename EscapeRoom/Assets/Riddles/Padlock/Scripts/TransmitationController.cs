7f0e4b5e138501458d5bc21ef2c7da10b2e2d3a1
Oruniarz <matimx2001@wp.pl>
2023-05-05 20:31:39 +0200

    Padlock com almost done

--- EscapeRoom/Assets/Riddles/Padlock/Scripts/TransmitationController.cs
+++ EscapeRoom/Assets/Riddles/Padlock/Scripts/TransmitationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEditor;
 
 public class TransmitationController : Riddle
 {
@@ -14,20 +15,27 @@ public class TransmitationController : Riddle
     public TMP_Text bt2;
     public TMP_Text bt3;
     public TMP_Text bt4;
-    private int a = 5, b = 1, c = 1, d = 1; //rozwiazanie - 5 1 2 4/5 2 1 4
+    private int a = 1, b = 1, c = 1, d = 1; //rozwiazanie - 5 1 2 4/5 2 1 4
+    private bool done = false;
+    private float total_perc = 0f;
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
+        base.Start();
         bt1.text = a.ToString();
         bt2.text = b.ToString();
         bt3.text = c.ToString();
         bt4.text = d.ToString();
 
         s_3.text = d.ToString();
-        s_2.text = (c*d+b*d).ToString();
-        s_1.text = (a+b*c*d).ToString();
+        s_2.text = (c * d + b * d).ToString();
+        s_1.text = (a + b * c * d).ToString();
         s_0.text = a.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
     }
     public override Delegate GetFunction(int index)
     {
@@ -56,98 +64,253 @@ public class TransmitationController : Riddle
 
     private void Increment1(float not_used)
     {
-        a++;
-        if(a>9)
+        if (!done)
         {
-            a = 1;
-        }
-        else if(a<1)
-        {
-            a = 9;
+            a++;
+            if (a > 9)
+            {
+                a = 1;
+            }
+            else if (a < 1)
+            {
+                a = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Decrement1(float not_used)
     {
-        a--;
-        if(a > 9)
+        if (!done)
         {
-            a = 1;
-        }
-        else if(a < 1)
-        {
-            a = 9;
+            a--;
+            if (a > 9)
+            {
+                a = 1;
+            }
+            else if (a < 1)
+            {
+                a = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Increment2(float not_used)
     {
-        b++;
-        if (b > 9)
+        if (!done)
         {
-            b = 1;
-        }
-        else if (b < 1)
-        {
-            b = 9;
+            b++;
+            if (b > 9)
+            {
+                b = 1;
+            }
+            else if (b < 1)
+            {
+                b = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Decrement2(float not_used)
     {
-        b--;
-        if (b > 9)
+        if (!done)
         {
-            b = 1;
-        }
-        else if (b < 1)
-        {
-            b = 9;
+            b--;
+            if (b > 9)
+            {
+                b = 1;
+            }
+            else if (b < 1)
+            {
+                b = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Increment3(float not_used)
     {
-        c++;
-        if (c > 9)
+        if (!done)
         {
-            c = 1;
-        }
-        else if (c < 1)
-        {
-            c = 9;
+            c++;
+            if (c > 9)
+            {
+                c = 1;
+            }
+            else if (c < 1)
+            {
+                c = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Decrement3(float not_used)
     {
-        c--;
-        if (c > 9)
+        if (!done)
         {
-            c = 1;
-        }
-        else if (c < 1)
-        {
-            c = 9;
+            c--;
+            if (c > 9)
+            {
+                c = 1;
+            }
+            else if (c < 1)
+            {
+                c = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Increment4(float not_used)
     {
-        d++;
-        if (d > 9)
+        if (!done)
         {
-            d = 1;
-        }
-        else if (d < 1)
-        {
-            d = 9;
+            d++;
+            if (d > 9)
+            {
+                d = 1;
+            }
+            else if (d < 1)
+            {
+                d = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
     }
     private void Decrement4(float not_used)
     {
-        d--;
-        if (d > 9)
+        if (!done)
         {
-            d = 1;
+            d--;
+            if (d > 9)
+            {
+                d = 1;
+            }
+            else if (d < 1)
+            {
+                d = 9;
+            }
+            bt1.text = a.ToString();
+            bt2.text = b.ToString();
+            bt3.text = c.ToString();
+            bt4.text = d.ToString();
+
+            s_3.text = d.ToString();
+            s_2.text = (c * d + b * d).ToString();
+            s_1.text = (a + b * c * d).ToString();
+            s_0.text = a.ToString();
+            if ((a == 5 && b == 1 && c == 2 && d == 4 && !done) || (a == 5 && b == 2 && c == 1 && d == 4 && !done))
+            {
+                done = true;
+                OnSolve();
+                total_perc = 1f;
+                GetSolvePercentage();
+            }
         }
-        else if (d < 1)
-        {
-            d = 9;
-        }
+    }
+    public override float GetSolvePercentage()
+    {
+        return total_perc;
     }
 }
